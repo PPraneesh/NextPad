ec2c8ff90eff001fefd572b1c2659a5237ea4ee6
Praneesh <parshipraneesh8@gmail.com>
2024-04-30 08:44:35 +0530

    url

--- FRONTEND/src/components/Root.jsx
+++ FRONTEND/src/components/Root.jsx
@@ -9,7 +9,7 @@ import { Outlet } from "react-router-dom";
 export default function Root(){
     const [loginStatus, setLoginStatus] = useState(false);
     const [user, setUser] = useState({})
-    const [url,setUrl] = useState("https://next-pad-backend.vercel.app/")
+    const [url,setUrl] = useState("https://nextpad-backend.vercel.app/")
     
     return(
         <div>
