e4cbe93e84a1355c991e6662445567039b7e7cb0
Praneesh <parshipraneesh8@gmail.com>
2024-05-02 11:52:50 +0530

    mongo implementation

--- BACKEND/api/index.js
+++ BACKEND/api/index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
-const mongoClient = require("mongodb").MongoClient;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const path = require('path');
 const cors = require('cors');
 require('dotenv').config();
 
 app.use(express.json());
 app.use(cors())
-
-mongoClient.connect(process.env.MONGODB_URI).then((client) => {
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    await client.connect();
   const db = client.db("webathonDB");
   const usersCollection = db.collection("users");
   const articlesCollection = db.collection("articles");
@@ -16,11 +24,13 @@ mongoClient.connect(process.env.MONGODB_URI).then((client) => {
   app.set("usersCollection", usersCollection);
   app.set("articlesCollection", articlesCollection);
   console.log("DB conncetion established");
-})
-.catch((err)=>{
-    console.log('Error connecting DB ', err);
-});
+}
+catch(err){
+  console.log(err);
 
+}
+}
+run().catch(console.dir);
 
 const userApp = require("./APIs/user-api.js");
 
